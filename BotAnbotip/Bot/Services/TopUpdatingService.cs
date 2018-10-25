afe4e3c02fdfbd91f8042e7e2857cb300828d63a
Andrey Tipapro <tipapro@hotmail.com>
2018-10-25 03:25:59 +0300

    .

--- BotAnbotip/Bot/Services/TopUpdatingService.cs
+++ BotAnbotip/Bot/Services/TopUpdatingService.cs
@@ -40,11 +40,12 @@ namespace BotAnbotip.Bot.Services
                     }
                     else
                     {
-                        for (int i = 0; i < DataManager.UserTopList.Value.Count - 1; i--)
+                        for (int i = DataManager.UserTopList.Value.Count - 1; i > 0; i--)
                         {
                             if (pair.Value.Points > DataManager.UserTopList.Value[i].Item2)
                             {
                                 DataManager.UserTopList.Value[i] = (pair.Key, pair.Value.Points, pair.Value.Level);
+                                break;
                             }
                         }
                     }
