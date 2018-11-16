36376a2ec39c26f9a9e2481175c139267a6df7fc
Andrey Tipapro <tipapro@hotmail.com>
2018-11-17 02:44:50 +0300

    .

--- BotAnbotip/Bot/Services/TopUpdatingService.cs
+++ BotAnbotip/Bot/Services/TopUpdatingService.cs
@@ -83,7 +83,7 @@ namespace BotAnbotip.Bot.Services
                     embedBuilder = embedBuilder.WithFooter("Последнее обновление: ").WithCurrentTimestamp();
 
                 if (messages.Count() <= i) await channel.SendMessageAsync("", false, embedBuilder.Build());
-                else await ((IUserMessage)messages[DataManager.UserTopList.Value.Count - i - 1]).ModifyAsync((prop) => { prop.Embed = embedBuilder.Build(); });
+                else await ((IUserMessage)messages[messages.Count() - i - 1]).ModifyAsync((prop) => { prop.Embed = embedBuilder.Build(); });
             }
             
         }
